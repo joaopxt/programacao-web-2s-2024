fd8ec6aa35d86c99faf3094a6e2b6aec2ce4da6d
Joao <peixoto.jvs@gmail.com>
2024-08-16 10:43:35 -0300

    Atividade 1 - Operadores

---
+++ helloWorld.js
@@ -0,0 +1,10 @@
+console.log('Hello World!')
+
+let v1 = 2;
+let v2 = 2;
+
+
+    console.log(`O resultado da operação de subtração entre ${v1} e ${v2} é: ${v1 - v2}`)
+    console.log(`O resultado da operação de adição entre ${v1} e ${v2} é: ${v1 + v2}`)
+    console.log(`O resultado da operação de multiplicação entre ${v1} e ${v2} é: ${v1 * v2}`)
+    console.log(`O resultado da operação de divisão entre ${v1} e ${v2} é: ${v1 / v2}`)
